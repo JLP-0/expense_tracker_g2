f8742c88e287581a7e4b02448887fcfe92caaf91
Jefferson Lopez Porras <jefferson.lopez.porras21@gmail.com>
2022-12-16 00:07:38 -0600

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewRecordComponent } from './components/new-record/new-record.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
   title = 'expense_tracker_g2';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { BudgetFormComponent } from './components/budget-form/budget-form.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { NewRecordComponent } from './components/new-record/new-record.component';
+import { RecordListComponent } from './components/record-list/record-list.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BudgetFormComponent,
+    ActionsComponent,
+    FooterComponent,
+    NewRecordComponent,
+    RecordListComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: 'app-new-record', component: NewRecordComponent},
+      {path: 'app-budget-form', component: BudgetFormComponent},
+      {path: '', component: BudgetFormComponent},
+    ]),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
